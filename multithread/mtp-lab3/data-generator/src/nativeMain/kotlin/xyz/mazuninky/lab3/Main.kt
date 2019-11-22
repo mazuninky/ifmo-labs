02852807701d7f45dc80cee0e34de3e68915fed5
Mazunin Konstantin <mazuninky@gmail.com>
2019-11-22 22:44:47 +0300

    Added new cli params for generator

--- multithread/mtp-lab3/data-generator/src/nativeMain/kotlin/xyz/mazuninky/lab3/Main.kt
+++ multithread/mtp-lab3/data-generator/src/nativeMain/kotlin/xyz/mazuninky/lab3/Main.kt
@@ -1,13 +1,19 @@
 package xyz.mazuninky.lab3
 
-import kotlin.random.Random
-
 const val MODE_OPTION = "mode"
 const val COUNT_OPTION = "count"
 
 const val MODE_UNIFORM = "uniform"
 const val UNIFORM_A_PARAM = "a"
 const val UNIFORM_B_PARAM = "b"
+
+const val MODE_СAUCHY = "сauchy"
+const val СAUCHY_X0_PARAM = "x0"
+const val СAUCHY_GAMMA_PARAM = "gamma"
+
+const val MODE_LOGISTIC = "logistic"
+const val LOGISTIC_MU_PARAM = "mu"
+const val LOGISTIC_S_PARAM = "s"
 
 fun main(args: Array<String>) {
     val parsed = parse(args)
@@ -17,6 +23,16 @@ fun main(args: Array<String>) {
             val a = checkNotNull(parsed.getFloatParam(UNIFORM_A_PARAM))
             val b = checkNotNull(parsed.getFloatParam(UNIFORM_B_PARAM))
             uniformGenerator(a, b)
+        }
+        MODE_СAUCHY -> {
+            val x0 = checkNotNull(parsed.getFloatParam(СAUCHY_X0_PARAM))
+            val gamma = checkNotNull(parsed.getFloatParam(СAUCHY_GAMMA_PARAM))
+            сauchyGenerator(x0, gamma)
+        }
+        MODE_LOGISTIC -> {
+            val mu = checkNotNull(parsed.getFloatParam(LOGISTIC_MU_PARAM))
+            val s = checkNotNull(parsed.getFloatParam(LOGISTIC_S_PARAM))
+            logisticGenerator(mu, s)
         }
         else -> throw IllegalArgumentException()
     }

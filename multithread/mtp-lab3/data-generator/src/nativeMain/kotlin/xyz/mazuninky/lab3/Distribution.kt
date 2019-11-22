508ac122e7f1ecbb1f1811ded991b897295e4cdd
Mazunin Konstantin <mazuninky@gmail.com>
2019-11-22 22:24:49 +0300

    Added Cauchy distribution #1

--- multithread/mtp-lab3/data-generator/src/nativeMain/kotlin/xyz/mazuninky/lab3/Distribution.kt
+++ multithread/mtp-lab3/data-generator/src/nativeMain/kotlin/xyz/mazuninky/lab3/Distribution.kt
@@ -4,8 +4,29 @@ import kotlin.random.Random
 
 typealias DistributionGenerator = () -> Float
 
-fun uniform(a: Float, b: Float): DistributionGenerator {
+fun uniform(a: Float, b: Float): Float {
+    return a + Random.nextFloat() * (b - a)
+}
+
+fun uniformGenerator(a: Float, b: Float): DistributionGenerator {
     return {
-        a + Random.nextFloat() * (b - a)
+        uniform(a, b)
+    }
+}
+
+fun сauchy(x0: Float, gamma: Float): Float {
+    var x: Float
+    var y: Float
+
+    do {
+        x = uniform(-1f, 1f)
+        y = uniform(-1f, 1f)
+    } while (x * x + y * y > 1.0 || y == 0f)
+    return x0 + gamma * x / y
+}
+
+fun сauchyGenerator(x0: Float, gamma: Float): DistributionGenerator {
+    return {
+        сauchy(x0, gamma)
     }
 }
